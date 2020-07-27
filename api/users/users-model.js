7e23e74f773e4768f214054d73c4d6f0110561de
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-27 10:28:32 -0500

    updated users-model

---
+++ api/users/users-model.js
@@ -0,0 +1,45 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+ find,
+ findById,
+ findClasses,
+ insert,
+ update,
+ remove
+};
+
+function find() {
+  return db('users');
+};
+
+function findById() {
+  return db('users').where({ id }).first();
+};
+
+function findClasses(userId) {
+  return db('classes')
+  //join user table with classes
+  //get the user id from users table and same user id from classes table
+  .join('users', 'users.id', 'classes.user_id')
+  //give me the class ID and the class Name under the title Upcoming Class
+  .select('classes.id', 'classes.name as Upcoming_Class')
+  //the class user ID should match the user ID
+  .where('classes.user_id', userId)
+};
+
+function insert(user) {
+  return db('users')
+  .insert(user)
+  .then(ids => {
+    return findById(ids[0]);
+  })
+};
+
+function update(id, changes) {
+  return db('users').where({ id }).update(changes);
+};
+
+function remove(id) {
+  return db('users').where('id', id).del();
+};
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
-const usersRouter = require('express').Router()
+const router = require('express').Router()
+
+const Users = require("./users-model.js")
+
+router.get("/users", (req,res) => {
+  Users.find()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => res.send(err));
+})
 
 
 
-
-
-
-module.exports = usersRouter
+module.exports = router
