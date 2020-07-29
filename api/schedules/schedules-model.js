10fb82d4326631bb9a95f8062fa317ff069e3470
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-29 06:33:49 -0500

    added table and fixed migration to include schedules table

---
+++ api/schedules/schedules-model.js
@@ -0,0 +1,49 @@
+const db = require('../../data/db-config.js')
+
+
+module.exports = {
+  find,
+  findBy,
+  findById,
+  insert,
+  update,
+  remove
+};
+
+//get class list
+function find() {
+  return db('classes').select('id', 'name').orderBy('id');
+};
+
+//get classes
+function findBy(filter) {
+  return db('classes').where(filter).orderBy('id')
+};
+
+//get classes by id
+function findById(id) {
+  return db('classes').where({ id }).first();
+};
+
+//create a new class
+function insert(newClass){
+  return db('classes')
+  .insert(newClass, 
+    ['id', 
+    'name', 
+    'description', 
+    'start',
+    'end', 
+    'trainer_id',
+    'user_id'])
+};
+
+//update classes
+function update(id, changes) {
+  return db('classes').where({ id }).update(changes);
+};
+
+//delete classes
+function remove (id) {
+  return db('classes').where('id', id).del();
+};
--- data/migrations/20200727220833_init.js
+++ data/migrations/20200727220833_init.js
@@ -3,41 +3,42 @@ exports.up = function(knex) {
   return knex.schema
   .createTable('users', tbl => {
     tbl.increments(); //ID auto primary key
-    tbl.text('username', 128).notNullable().unique(); //required unique username
+    tbl.text('username', 128).notNullable().unique(); //required username
     tbl.text('password', 255).notNullable(); //required password
-    tbl.text('email', 255).notNullable().unique(); //required unique email
+    tbl.text('email', 255).notNullable().unique(); //required email
     tbl.text('bio', 255)
   })
-
   .createTable('trainers', tbl => {
     tbl.increments(); //ID auto primary key
-    tbl.text('name', 128).notNullable().unique(); //required unique name
+    tbl.text('name', 128).notNullable().unique(); //required name
     tbl.text('password', 255).notNullable(); //required password
-    tbl.text('email', 255).notNullable().unique(); //required unique email
+    tbl.text('email', 255).notNullable().unique(); //required email
     tbl.text('bio', 255)
   })
-
   .createTable('classes', tbl => {
     tbl.increments(); //ID auto primary key
-    tbl.text('name', 128).notNullable();
+    tbl.text('name', 128).notNullable().unique();
     tbl.text('description', 255).notNullable();
     tbl.text('start', 7).notNullable();
     tbl.text('end', 7).notNullable();
-    tbl.integer('user_id')//user ID auto incremented
-      .unsigned()//never negative
-      .references('id')//id primary key column
-      .inTable('users') //users table
     tbl.integer('trainer_id')// trainer id auto incremented
       .unsigned()
       .references('id')
       .inTable('trainers')
+  })
+  .createTable('schedules', tbl => {
+    tbl.increments();
+    tbl.integer('class_id').unsigned().references('id').inTable('classes')
+    tbl.integer('trainer_id').unsigned().references('id').inTable('trainers')
+    tbl.integer('user_id').unsigned().references('id').inTable('users')
   })
   
 };
 
 exports.down = function(knex) {
   return knex.schema
-  .dropTableIfExists('classes') // last table created
-  .dropTableIfExists('trainers')// 2nd table created
-  .dropTableIfExists('users') // 1st table created
+  .dropTableIfExists('schedules') // 4th created
+  .dropTableIfExists('classes') // 3rd created
+  .dropTableIfExists('trainers')// 2nd created
+  .dropTableIfExists('users') // 1st created
 };
