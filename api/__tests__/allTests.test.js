622f1bcd104e0afcdd3b4ee3068b0f26166db8b4
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-07-30 15:13:54 -0500

    starting testing

---
+++ api/__tests__/allTests.test.js
@@ -0,0 +1,138 @@
+//import what I need
+
+const auth = require('../auth/auth-router.js')
+const supertest = require('supertest')
+const server = require('../../server.js');
+
+
+//have an array of a dummy list of users, trainers, classes, schedules
+
+const users = [
+  {
+    id: 1,
+    username: 'usertest1',
+    password: 'abcde',
+    email: 'user@test.com',
+    bio: 'bio here',
+    schedule: {
+        class1: "class 1",
+        class2: "class2",
+    }
+  
+  },
+  {
+    id: 2,
+    username: 'usertest2',
+    password: 'abcd',
+    email: 'user2@test.com',
+    schedule: {
+      class1: "class 1",
+  }
+  },
+  {
+    id: 3,
+    username: 'usertest3',
+    password: 'abcde',
+    email: 'user@test.com',
+    schedule: {}
+  },
+];
+
+const trainers = [
+  {
+    id: 1,
+    name: 'trainertest1',
+    password: 'abcde',
+    email: 'trainer@test.com',
+    bio: 'bio here'
+  },
+  {
+    id: 2,
+    name: 'trainertest2',
+    password: 'abcd',
+    email: 'trainer2@test.com',
+  },
+  {
+    id: 3,
+    name: 'trainertest3',
+    password: 'abcde',
+    email: 'trainer3@test.com',
+    bio: null
+  },
+];
+
+const classes = [
+  {
+    id: 1,
+    name: 'test class',
+    description: 'description here',
+    start: '5PM',
+    end: '6PM',
+    trainer_id: 2
+  },
+  {
+    id: 2,
+    name: 'test class2',
+    description: 'description 2 here',
+    start: '5PM',
+    end: '6PM',
+    trainer_id: null
+  },
+];
+
+const schedules = [
+  {
+    id: 1,
+    name: 'test class',
+    description: 'description here',
+    start: '5PM',
+    end: '6PM',
+    trainer_id: 2
+  },
+  {
+    id: 2,
+    name: 'test class2',
+    description: 'description 2 here',
+    start: '5PM',
+    end: '6PM',
+    trainer_id: 3
+  },
+]
+
+/* -*_*_*_*_*_API_*_*_*_*_*_*- */
+
+describe('api is up', () => {
+  test.todo('api success message')
+})
+
+/* -*_*_*_*_* SCHEDULES *_*_*_*_*_*- */
+describe('view schedules', () => {
+  test.todo('anyone can see scheduled classes')
+  test.todo('get schedule by id')
+})
+
+
+
+/* -*_*_*_*_* AUTH *_*_*_*_*_*- */
+describe('not authorized', () => {
+  test.todo('not auth message')
+})
+
+/* -*_*_*_*_* USERS / TRAINERS LOGIN (SAME) *_*_*_*_*_*- */
+describe('register user/trainer', () => {
+  test.todo('make new user/trainer')
+  test.todo('login user/trainer success')
+  test.todo('not unique username/email fail message')
+  test.todo('update profile user/trainer')
+  test.todo('users can see schedule by id')
+})
+
+/* -*_*_*_*_* TRAINERS / CLASSES *_*_*_*_*_*- */
+describe('trainer and classes', () => {
+  test.todo('add a new class')
+  test.todo('update a class')
+  test.todo('delete a class')
+})
+
+
+
